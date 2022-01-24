f4df83b87e4a6de41af4f9bd00a611b468cba0ee
QuinnLeavitt <qwe42@live.missouristate.edu>
2022-01-23 23:49:54 -0600

    Generate PDF

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,13 +57,19 @@ const darkTheme = 'dark-theme'
 const iconTheme = 'bx-sun'
 
 /*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
-
+function scaleResume() {
+    document.body.classList.add('scale-cv')
+}
 
 /*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/
-
+function removeScale() {
+    document.body.classList.remove('scale-cv')
+}
 
 /*==================== GENERATE PDF ====================*/
 // PDF generated area
+
+let resumeButton = document.getElementById('resume-button')
 
 
 // Html2pdf options
@@ -73,11 +79,13 @@ const iconTheme = 'bx-sun'
 
 
 // When the button is clicked, it executes the three functions
-
+resumeButton.addEventListener('click', () => {
     // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-
+    scaleResume()
 
     // 2. The PDF is generated
 
 
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+
+})
